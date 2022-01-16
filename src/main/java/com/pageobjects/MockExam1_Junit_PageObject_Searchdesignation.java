7d181311aa196cb2ab7415d27c354d9af1e1cb9e
Sushma0905 <67358251+Sushma0905@users.noreply.github.com>
2022-01-16 13:24:42 +0530

    Update MockExam1_Junit_PageObject_Searchdesignation.java

--- src/main/java/com/pageobjects/MockExam1_Junit_PageObject_Searchdesignation.java
+++ src/main/java/com/pageobjects/MockExam1_Junit_PageObject_Searchdesignation.java
@@ -7,5 +7,8 @@ import com.testing.base.TestBase;
 public class MockExam1_Junit_PageObject_Searchdesignation extends TestBase{
 	
 	public By Menu = By.xpath("//li[contains(@class,'slideout-toggle')]//a");
+	
+	public By m = By.xpath("");
+	public By ne = By.xpath("");
 
 }
